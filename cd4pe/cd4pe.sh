73c0ff1f6d2ee4950f33c78977d15bf6237b9441
boltKrank <simon.anderson111@gmail.com>
2018-12-17 16:22:25 +1100

    Update cd4pe.sh

--- cd4pe/cd4pe.sh
+++ cd4pe/cd4pe.sh
@@ -3,13 +3,13 @@ docker run --rm --name artifactory -v /pipelines/artifactory:/var/opt/jfrog/arti
 echo "Please log in to Artifactory and setup a generic-local db"
 read -n 1 -s -r -p "Press any key to continue"
 
-response=$(curl -u admin:password http://localhost:8081/artifactory/api/security/users/apiKey)
-response=$(curl -X POST -u admin:password http://localhost:8081/artifactory/api/security/apiKey)
+response=$(curl -u admin:puppetlabs http://localhost:8081/artifactory/api/security/users/apiKey)
+response=$(curl -X POST -u admin:puppetlabs http://localhost:8081/artifactory/api/security/apiKey)
 echo "APIKEY = $response"
 
 echo "Starting MYSQL"
-docker run --rm --name mysql -v /pipelines/mysql:/var/lib/mysql --env-file mysql.env -p 3306:3306 -d mysql/mysql-server:5.7
+docker run --rm --name mysql -v /cd4pe/mysql:/var/lib/mysql --env-file mysql.env -p 3306:3306 -d mysql/mysql-server:5.7
 echo "Waiting for 5"
 sleep 5
-echo "Starting PFA"
-docker run --rm --name pfa --env-file pipelines.env -p 8080:8080 -p 8000:8000 -p 7000:7000 -d puppet/pipelines-for-applications:latest
+echo "Starting CD4PE"
+docker run --rm --name pfa --env-file cd4pe.env -p 8080:8080 -p 8000:8000 -p 7000:7000 -d puppet/continuous-delivery-for-puppet-enterprise:latest
